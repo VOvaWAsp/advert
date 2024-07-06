a625f054e345286a00829fe8ff5cb584b986d729
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-07-06 11:37:55 +0300

    filter (vehicle equipment and vehicle type)

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,27 +1,62 @@
-import { useState } from "react";
-import Catalog from "../Catalog/Catalog";
-import Filter from "../Filter/Filter";
-import { useSelector } from "react-redux";
+import React, { useState } from 'react';
+import Catalog from '../Catalog/Catalog';
+import Filter from '../Filter/Filter';
+import { useSelector } from 'react-redux';
 
 function CatalogList() {
-    const [filter, setFilter] = useState('');
-    const catalogs = useSelector(state => state.catalog.items);
+    const [filterLocation, setFilterLocation] = useState('');
+    const [filterCheckBox, setFilterCheckBox] = useState({
+        airConditioner: false,
+        automatic: false,
+        kitchen: false,
+        TV: false,
+        shower: false
+    });
+    const [vehicleType, setVehicleType] = useState('');
 
-    const handleFilterChange = (value) => {
-        setFilter(value)
+    const catalogs = useSelector((state) => state.catalog.items);
+
+    const handleFilterLocationChange = (value) => {
+        setFilterLocation(value);
     };
 
-    const filteredCatalogs = catalogs.filter(catalog => 
-        catalog.location.toLowerCase().includes(filter.toLowerCase())
-        )
+    const handleFilterCheckBoxChange = (options) => {
+        setFilterCheckBox({
+            ...filterCheckBox,
+            ...options
+        });
+    };
+
+    const handleFilterRadioChange = (value) => {
+        setVehicleType(value);
+    };
+
+    console.log(vehicleType)
+
+    const filteredCatalogs = catalogs.filter((catalog) => {
+        const location = catalog.location.toLowerCase().includes(filterLocation.toLowerCase());
+        const equipment =
+            (!filterCheckBox.airConditioner || catalog.details.airConditioner > 0) &&
+            (!filterCheckBox.automatic || catalog.transmission === 'automatic') &&
+            (!filterCheckBox.kitchen || catalog.details.kitchen > 0) &&
+            (!filterCheckBox.TV || catalog.details.TV > 0) &&
+            (!filterCheckBox.shower || catalog.details.shower > 0);
+            const type = vehicleType === '' || catalog.form === vehicleType;
+
+        return location && equipment && type;
+    });
 
     return (
-    <div>
-        <Filter onFilterChanges={handleFilterChange} />
-        <ul>
-            <Catalog filteredCatalogs={filteredCatalogs} />
-        </ul>
-    </div>
+        <div>
+            <Filter
+                onFilterCheckBoxChanges={handleFilterCheckBoxChange}
+                onFilterLocationChanges={handleFilterLocationChange}
+                onFilterRadioChanges={handleFilterRadioChange}
+            />
+            <ul>
+                <Catalog filteredCatalogs={filteredCatalogs} />
+            </ul>
+        </div>
     );
 }
 
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,145 @@
-function Filter({onFilterChanges}) {
+import { useState } from 'react';
+import styles from './Filter.module.css'; 
+
+function Filter({ onFilterLocationChanges, onFilterCheckBoxChanges, onFilterRadioChanges }) {
+    const [filters, setFilters] = useState({
+        airConditioner: false,
+        automatic: false,
+        kitchen: false,
+        TV: false,
+        shower: false
+    });
+    const [vehicleType, setVehicleType] = useState('');
+
     const filterLocation = (event) => {
-        onFilterChanges(event.target.value)
-    }
+        onFilterLocationChanges(event.target.value);
+    };
+
+    const handleCheckBoxChange = (event) => {
+        const { name, checked } = event.target;
+        setFilters({ ...filters, [name]: checked });
+    };
+
+    const handleRadioChange = (event) => {
+        const { value } = event.target;
+        setVehicleType(value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const filterVehicleType = vehicleType === '' ? null : vehicleType;
+
+        onFilterCheckBoxChanges(filters);
+        onFilterRadioChanges(filterVehicleType);
+    };
+
     return (
         <>
-        <input onChange={filterLocation} type="text" name="location" />
+            <input
+                className={styles.filterInput}
+                onChange={filterLocation}
+                type="text"
+                name="location"
+            />
+            <form className={styles.filterForm} onSubmit={handleSubmit}>
+                <div className={styles.filters}>
+                    <h3 className={styles.filterTitle}>Filters</h3>
+                    <div className={styles.filterSection}>
+                        <h2>Vehicle equipment</h2>
+                        <div className={styles.checkboxGroup}>
+                            <label className={styles.checkboxLabel}>
+                                Air Conditioner
+                                <input
+                                    className={styles.checkboxInput}
+                                    onChange={handleCheckBoxChange}
+                                    name="airConditioner"
+                                    type="checkbox"
+                                    checked={filters.airConditioner}
+                                />
+                            </label>
+                            <label className={styles.checkboxLabel}>
+                                Automatic
+                                <input
+                                    className={styles.checkboxInput}
+                                    onChange={handleCheckBoxChange}
+                                    name="automatic"
+                                    type="checkbox"
+                                    checked={filters.automatic}
+                                />
+                            </label>
+                            <label className={styles.checkboxLabel}>
+                                Kitchen
+                                <input
+                                    className={styles.checkboxInput}
+                                    onChange={handleCheckBoxChange}
+                                    name="kitchen"
+                                    type="checkbox"
+                                    checked={filters.kitchen}
+                                />
+                            </label>
+                            <label className={styles.checkboxLabel}>
+                                TV
+                                <input
+                                    className={styles.checkboxInput}
+                                    onChange={handleCheckBoxChange}
+                                    name="TV"
+                                    type="checkbox"
+                                    checked={filters.TV}
+                                />
+                            </label>
+                            <label className={styles.checkboxLabel}>
+                                Shower/WC
+                                <input
+                                    className={styles.checkboxInput}
+                                    onChange={handleCheckBoxChange}
+                                    name="shower"
+                                    type="checkbox"
+                                    checked={filters.shower}
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className={styles.filterSection}>
+                        <h2>Vehicle type</h2>
+                        <div className={styles.radioGroup}>
+                            <label className={styles.radioLabel}>
+                            Van
+                                <input
+                                    className={styles.radioInput}
+                                    onChange={handleRadioChange}
+                                    value='panelTruck'
+                                    name="vehicleType"
+                                    type="radio"
+                                />
+                            </label>
+                            <label className={styles.radioLabel}>
+                            Fully Integrated
+                                <input
+                                    className={styles.radioInput}
+                                    onChange={handleRadioChange}
+                                    value='fullyIntegrated'
+                                    name="vehicleType"
+                                    type="radio"
+                                />
+                            </label>
+                            <label className={styles.radioLabel}>
+                            Alcove
+                                <input
+                                    className={styles.radioInput}
+                                    onChange={handleRadioChange}
+                                    value='alcove'
+                                    name="vehicleType"
+                                    type="radio"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <button type='submit' className={styles.filterButton}>Apply Filters</button>
+                </div>
+            </form>
         </>
-    )
+    );
 }
 
 export default Filter;
