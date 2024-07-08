6f2e667ee82eac3e27a73b11368ad8cebba2b381
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-07-08 22:40:49 +0300

    finished

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,6 +1,15 @@
 import css from "./Favorites.module.css";
+import sprite from "../../sprite/sprite.svg"
+import { useEffect, useState } from "react";
 
 function Favorites() {
+    const [favoriteItems, setFavoriteItems] = useState([]);
+
+    useEffect(() => {
+        const savedItems = JSON.parse(window.localStorage.getItem('saved')) || [];
+        setFavoriteItems(savedItems);
+    }, []);
+
     const items = JSON.parse(window.localStorage.getItem('saved')) || [];
 
     const truncateText = (text, maxLength) => {
@@ -10,19 +19,29 @@ function Favorites() {
         return text;
     };
 
+    useEffect(() => {
+        const savedItems = JSON.parse(window.localStorage.getItem('saved')) || [];
+        setFavoriteItems(savedItems);
+    }, []);
+
     const handleFavorites = (item) => {
         const savedItems = JSON.parse(window.localStorage.getItem('saved')) || [];
-
         const itemIndex = savedItems.findIndex(savedItem => savedItem._id === item._id);
 
-        if (itemIndex !== -1) {            savedItems.splice(itemIndex, 1);
+        if (itemIndex !== -1) {
+            savedItems.splice(itemIndex, 1);
         } else {
             savedItems.push(item);
         }
 
         window.localStorage.setItem('saved', JSON.stringify(savedItems));
-        window.location.reload(); 
+        setFavoriteItems(savedItems);
     };
+
+    const isFavorite = (itemId) => {
+        return favoriteItems.some(favItem => favItem._id === itemId);
+    };
+
 
     return (
         <div>
@@ -32,8 +51,9 @@ function Favorites() {
             <div>
                 <h2 className={css.favoritesTitle}>Your favorite cars</h2>
                 <ul className={css.list}>
-                    {items.map(item => (
-                        <li className={css.item} key={item._id}>
+                    {items.map(item => {
+                                         const isItemFavorite = isFavorite(item._id);
+                      return ( <li className={css.item} key={item._id}>
                             <div className={css.container}>
                                 <img className={css.img} src={item.gallery[0]} alt="" />
                                 <div className={css.blockInfo}>
@@ -41,8 +61,13 @@ function Favorites() {
                                         <div className={css.blockInfoPrice}>
                                             <h2 className={css.title}>{item.name}</h2>
                                             <h3 className={css.price}>€{item.price}</h3>
-                                            <button onClick={() => handleFavorites(item)}>Unfavorite</button>
-                                        </div>
+                                            <button className={css.buttonFavorite} onClick={() => handleFavorites({ ...item })}>
+                                            {isItemFavorite ? <svg width="25" height="24">
+                                                <use fill="white" stroke="black" href={`${sprite}#icon-Property-1Default`}></use>
+                                            </svg> : <svg width="25" height="24">
+                                                <use href={`${sprite}#icon-Property-1pressed`}></use>
+                                            </svg>}
+                                        </button>                                        </div>
                                         <div className={css.RataAndLocal}>
                                             <h3 className={css.rate}>{item.rating} ({item.reviews.length} Reviews)</h3>
                                             <h3 className={css.location}>{item.location}</h3>
@@ -73,7 +98,7 @@ function Favorites() {
                                 </div>
                             </div>
                         </li>
-                    ))}
+                    )})}
                 </ul>
             </div>
             )}
